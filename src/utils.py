8189b1e10286646fbe882c662a0058c3a79c5d28
= <=>
2024-03-01 09:48:48 +0100

    docstrings

--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,13 @@ import logging
 
 
 # Constantes empleadas en el uso del servicio
-SERVER_IP = ""
-SERVER_PORT = ""
-SERVER_HOSTNAME = ""
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5001
+SERVER_HOSTNAME = "COLISEUM"
 
 
-def configure_logging(file_log_name):
+def configure_logging(file_log_name:str)->logging.Logger:
+    """Returns a Logger object with custom attibutes for the service"""
 
     # Defininig the logger folder
     os.getcwd()
@@ -28,8 +29,10 @@ def configure_logging(file_log_name):
     logger.addHandler(fh)
     return logger
 
+logger = configure_logging("messages.log")
 
 class Message:
+    """Class use for manipulating messages between server and clients and viceversa"""
 
     def __init__(self, sender_id = None, status = 200, content = None) -> None:
         self.sender_id:str = sender_id
@@ -40,9 +43,11 @@ class Message:
         try:
             return json.dumps(self.__dict__).encode("utf-8")
         except Exception as error:
+            logger.error(error)
             raise
 
     def print(self):
+        """Display the message send by a client in the console of the other client"""
         print(f"{self.sender_id}:{self.content}")
 
     @staticmethod
@@ -55,6 +60,6 @@ class Message:
             content = data.get("content")
             return Message(sender_id=sender_id, status=status, content=content)
         except Exception as error:
+            logger.error(error)
             raise
-    
     
