23898dd5472c6744b4e189475bd87ed6671b1980
= <=>
2024-03-19 18:04:39 +0100

    cosas

---
+++ coliseum-client/src/main.rs
@@ -0,0 +1,13 @@
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Connect to the server
+    let mut stream = TcpStream::connect("127.0.0.1:5000").await?;
+
+    // Write data to the server
+    stream.write_all(b"Hello from client!\n").await?;
+    stream.flush().await?;
+    Ok(())
+}
--- coliseum-net/src/main.rs
+++ coliseum-net/src/main.rs
@@ -1,25 +1,26 @@
 mod node;
-mod req;
+
+use node::Node;
+
+use tokio::{io::BufStream, net::TcpListener};
 
 use std::{
     collections::HashSet,
-    sync::{Arc,RwLock}
+    sync::{Arc, RwLock}
 };
 
-use tokio::{self, io::BufStream, net::TcpListener};
-use node::Node;
+
 
 
 
 #[tokio::main]
-async fn main(){
-
-    let port = 5000;    
+async fn main() {
+    let port = 5000;
 
     let mut addr = "127.0.0.1:".to_string();
     addr.push_str(&port.to_string());
 
-    let mut node = Node{
+    let mut node = Node {
         addr,
         register_addr: "http://127.0.0.1:5000".to_string(),
         nodes: Arc::new(RwLock::new(HashSet::new())),
@@ -30,25 +31,17 @@ async fn main(){
         node.register_node_in_entrypoint().await;
     }
 
+
     let listener = TcpListener::bind(node.addr.clone()).await.unwrap();
-    println!("listening on: {}", listener.local_addr().unwrap());
 
     loop {
         let (stream, addr) = listener.accept().await.unwrap();
-        let mut stream = BufStream::new(stream);
 
-        // do not block the main thread, spawn a new task
         tokio::spawn(async move {
-            println!("New connection {}", addr);
-
-            match req::parse_request(&mut stream).await {
-                Ok(_) => println!("Incoming request"),
-                Err(error) => {
-                    println!("failed to parse request: {}", error);
-                }
-            }
+            let buf = [0,1024];
+            let stream = BufStream::new(stream);
+    
         });
     }
 
 }
-
