8ee918bbf2d34883c2f807ef1be35696ed16abb8
= <=>
2024-04-08 17:32:33 +0200

    cosas

--- coliseum-net/src/main.rs
+++ coliseum-net/src/main.rs
@@ -1,5 +1,5 @@
 use std::time::{SystemTime, UNIX_EPOCH};
-
+use std::net::TcpStream;
 use uuid::Uuid;
 
 #[derive(Debug, Clone)]
@@ -53,7 +53,7 @@ impl Server {
         timestamp
     }
 
-    /// Create a new account using the user_name
+    /// Static -> Create a new account using the user_name
     pub fn create_account(&mut self, user_name: String) -> Account {
         let timestamp = Server::create_timestamp();
         let account = Account {
@@ -67,7 +67,7 @@ impl Server {
         account
     }
 
-    /// Get an specific account query by its account ID
+    /// Dyanamic -> Get an specific account query by its account ID
     pub fn get_account(&mut self, account_id: &str) -> Result<&mut Account, ()> {
         for account in self.accounts.iter_mut() {
             if account_id == account.id {
@@ -80,7 +80,7 @@ impl Server {
         Err(())
     }
 
-    /// Create a new transaction but it is not check
+    ///Static -> Create a new transaction but it is not check
     pub fn create_transaction(
         &mut self,
         from_id: String,
@@ -99,12 +99,37 @@ impl Server {
         transaction
     }
 
-    /// Get all the transaction
-    pub fn get_transactions(self) -> Vec<Transaction> {
+    /// Static -> Get all transactions stored in Server
+    pub fn get_all_transactions(self) -> Vec<Transaction> {
         self.transactions.clone()
     }
 
-    /// Check if a transaction is valid
+    // Static -> Get an specific transaction query by ID
+    pub fn get_transaction(self, transaction_id: String) -> Result<Transaction, ()> {
+        for transaction in self.transactions.clone() {
+            if transaction_id == transaction.id {
+                return Ok(transaction);
+            } else {
+                continue;
+            }
+        }
+        return Err(());
+    }
+
+    // Static -> Get an specific transaction query by ID
+    pub fn get_transaction_by_account(self, account_id: String) -> Vec<Transaction> {
+        let mut transactions = Vec::new();
+        for transaction in self.transactions.clone() {
+            if account_id == transaction.from_id {
+                transactions.push(transaction)
+            } else {
+                continue;
+            }
+        }
+        transactions
+    }
+
+    /// Static -> Check if a transaction is valid
     pub fn check_transaction(&mut self, transaction: Transaction) -> bool {
         let account_from = self.get_account(&transaction.from_id);
 
@@ -141,7 +166,63 @@ impl Server {
         self.transactions.push(transaction.clone());
         true
     }
+
+
 }
+
+enum Endpoint {
+    GetAccount, // Recupera la información de la cuenta y de las transacciones asociadas a la cuenta
+    CreateAccount, // Crea una cuenta nueva
+    //GetAllTransactions, // Recupera todas las transacciones alojadas en el Servidor
+    //GetTransaction, // Obtiene una transacción concreta asociado a un ID
+    //GetTransactions, // Obtiene las transacciones asocidas a una cuenta
+    CreateTransaction // Crea una neuva transacción y la chequea
+}
+
+pub struct  DataCreateAccount {
+    username: String
+}
+
+pub struct DataGetAccount{
+    account_id:String
+}
+pub struct DataCreateTransaction{
+    from_id: String,
+    to_id: String,
+    amount: f64,
+}
+
+
+pub struct Request {
+    endpoint: Endpoint,
+    orgin_addr: String,
+    target_addr: String,
+    data: <DataCreateAccount, DataGetAccount, DataCreateTransaction>,
+}
+
+
+struct View{}
+
+impl View{
+    pub fn handle_connection(stream: &mut TcpStream, request: Request) {
+        
+        match request.endpoint {
+            Endpoint::CreateAccount => {
+                let data: Option<DataCreateAccount> = request.data;
+                
+            }
+
+            Endpoint::GetAccount => {
+                let data: Option<DataGetAccount> = request.data;
+            }
+
+            Endpoint::CreateTransaction => {
+                let data: Option<DataCreateTransaction> = request.data;
+            }
+        }
+    }    
+}
+
 
 fn main() {
     let mut server = Server::new("127.0.0.1:5000".to_string());
@@ -176,7 +257,7 @@ fn main() {
         account.id, account.balance
     );
 
-    for item in server.get_transactions().clone() {
+    for item in server.get_all_transactions().clone() {
         println!(
             "ID:{} from {} to {} amount:{} ",
             item.id, item.from_id, item.to_id, item.amount
